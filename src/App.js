0d6098fac752ef894b109468969bcffb11ab56c3
zachsilberstein <zjs224@lehigh.edu>
2022-11-15 20:34:48 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 function App() {
+  const [unformatted, setUnformatted] = useState("");
+  const [formatted, setFormatted] = useState("");
+
+  const format = () => {
+    console.log("FORMATTING");
+    console.log("un" + unformatted);
+    setFormatted(unformatted);
+  };
+
+  const updateUnformatted = (event) => {
+    setUnformatted(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="m-5">
+          <label>Unformatted Menu</label>
+          <textarea
+            id="input"
+            className="form-control"
+            onKeyUp={updateUnformatted}
+          />
+          <button className="btn btn-primary" onClick={format}>
+            Format!
+          </button>
+        </div>
+        <div className="m-5">
+          <label>Formatted Menu</label>
+          <textarea id="output" className="form-control" value={formatted} />
+        </div>
+      </div>
     </div>
   );
 }
