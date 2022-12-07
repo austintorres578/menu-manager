a7de39838ee8c879c4737ef59ac8ac56473b8c0b
zachsilberstein <zjs224@lehigh.edu>
2022-12-07 15:04:49 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
-import { findByAltText } from "@testing-library/react";
 
 function App() {
   const [unformatted, setUnformatted] = useState("");
   const [formatted, setFormatted] = useState("");
   const [update, setUpdate] = useState(false);
+  const [formatter, setFormatter] = useState(
+    "<p><b>[name]</b>[price]<br>[description]</p>\n"
+  );
+
+  const [optionsMenu, toggleOptions] = useState(false);
 
   const format = () => {
     console.log("FORMATTING");
@@ -50,19 +54,17 @@ function App() {
         }
         if (lines[i].includes("$")) {
           price += lines[i];
-          final.items +=
-            "<p><b>" +
-            name +
-            "</b> " +
-            price +
-            "<br>" +
-            (description ? description + "</p>\n" : "</p>\n");
+          final.items += formatter
+            .replace("[name]", name)
+            .replace("[price]", price)
+            .replace("[description]", description);
           name = "";
           price = "";
           description = "";
         }
       }
     }
+    sections.push({ heading: final.heading, items: final.items });
     console.log(sections);
     setFormatted(sections);
     setUpdate(!update);
@@ -72,21 +74,55 @@ function App() {
     setUnformatted(event.target.value);
   };
 
+  const updateFormatter = (event) => {
+    setFormatter(event.target.value);
+  };
+
   return (
     <div className="App">
       <div className="container">
-        <div className="m-5">
+        <div className="row mt-5">
+          <div className="col-1">
+            <button
+              className="btn btn-primary"
+              onClick={() => {
+                toggleOptions(!optionsMenu);
+              }}
+            >
+              {optionsMenu ? <>&#x25B2;</> : <>&#x25BC;</>}
+            </button>
+          </div>
+          {optionsMenu && (
+            <div className="col-11">
+              <div className="col-6">
+                <label>Formatter Output:</label>
+                <br />
+                <textarea row="3" value={formatter} disabled className="m-1" />
+                <textarea
+                  id="output"
+                  rows="10"
+                  className="form-control"
+                  onKeyUp={updateFormatter}
+                />
+              </div>
+              <div className="col-6"></div>
+            </div>
+          )}
+        </div>
+        <div className="m-1">
           <label>Unformatted Menu</label>
           <textarea
             id="input"
-            className="form-control p-5"
+            rows="10"
+            style={{ height: "100%" }}
+            className="form-control"
             onKeyUp={updateUnformatted}
           />
           <button className="btn btn-primary" onClick={format}>
             Format!
           </button>
         </div>
-        <div className="m-5">
+        <div className="m-1">
           <label>Formatted Menus</label>
           {formatted &&
             formatted.map((section) => {
@@ -95,7 +131,9 @@ function App() {
                   <h6>{section.heading}</h6>
                   <textarea
                     id="output"
-                    className="form-control p-5"
+                    rows="10"
+                    style={{ height: "100%" }}
+                    className="form-control"
                     value={section.items}
                   />
                 </div>
